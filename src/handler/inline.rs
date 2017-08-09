f115587bd95fc066c7afa9ad051285127731f44b
Gosuke Miyashita <gosukenator@gmail.com>
2017-08-09 16:42:10 +0900

    Remove unused trait and method

--- src/handler/inline.rs
+++ src/handler/inline.rs
@@ -1,11 +1,2 @@
-use handler::Handler;
-use platform::platform::Platform;
-
 #[derive(Debug)]
 pub struct Inline;
-
-impl Handler for Inline {
-    fn detect_platform(&self, f: &Fn() -> Option<Box<Platform>>) -> Option<Box<Platform>> {
-        f()
-    }
-}
--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,8 +1,1 @@
-use std::fmt::Debug;
-use platform::platform::Platform;
-
-pub trait Handler: Debug {
-    fn detect_platform(&self, f: &Fn() -> Option<Box<Platform>>) -> Option<Box<Platform>>;
-}
-
 pub mod inline;
