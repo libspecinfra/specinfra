3a8430fa322946a37733d434d752999c4cf4c6c7
Gosuke Miyashita <gosukenator@gmail.com>
2017-09-19 16:59:59 +0900

    Allow to get error description via FFI wrapper functions
    * Add error field to File struct
    * Define resource_file_error_description() to get error description
    * use std::error::Error to use trait method

--- src/resource/file.rs
+++ src/resource/file.rs
@@ -1,15 +1,18 @@
 use backend::Backend;
-use provider::error::Error;
+use provider::error;
 use provider::Provider;
 use provider::Output;
 use libc::{c_char, uint32_t, int64_t};
 use std::ffi::CString;
 use std::ffi::CStr;
+use std;
+use std::error::Error;
 
 pub struct File<'a> {
     name: &'static str,
     backend: &'a Backend,
     provider: &'a Provider,
+    error: Option<error::Error>,
 }
 
 impl<'a> File<'a> {
@@ -18,160 +21,161 @@ impl<'a> File<'a> {
             name: n,
             backend: b,
             provider: p,
+            error: None,
         }
     }
 
-    pub fn mode(&self) -> Result<u32, Error> {
+    pub fn mode(&self) -> Result<u32, error::Error> {
         self.backend
             .handle(self.provider.file.mode(self.name))
             .and_then(Output::to_u32)
     }
 
-    pub fn size(&self) -> Result<i64, Error> {
+    pub fn size(&self) -> Result<i64, error::Error> {
         self.backend
             .handle(self.provider.file.size(self.name))
             .and_then(Output::to_i64)
     }
 
-    pub fn is_file(&self) -> Result<bool, Error> {
+    pub fn is_file(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_file(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn exist(&self) -> Result<bool, Error> {
+    pub fn exist(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.exist(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_directory(&self) -> Result<bool, Error> {
+    pub fn is_directory(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_directory(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_block_device(&self) -> Result<bool, Error> {
+    pub fn is_block_device(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_block_device(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_character_device(&self) -> Result<bool, Error> {
+    pub fn is_character_device(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_character_device(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_pipe(&self) -> Result<bool, Error> {
+    pub fn is_pipe(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_pipe(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_socket(&self) -> Result<bool, Error> {
+    pub fn is_socket(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_socket(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_symlink(&self) -> Result<bool, Error> {
+    pub fn is_symlink(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_symlink(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn contents(&self) -> Result<String, Error> {
+    pub fn contents(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.contents(self.name))
             .and_then(Output::to_string)
     }
 
-    pub fn owner(&self) -> Result<String, Error> {
+    pub fn owner(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.owner(self.name))
             .and_then(Output::to_string)
     }
 
-    pub fn group(&self) -> Result<String, Error> {
+    pub fn group(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.group(self.name))
             .and_then(Output::to_string)
     }
 
-    pub fn linked_to(&self) -> Result<String, Error> {
+    pub fn linked_to(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.linked_to(self.name))
             .and_then(Output::to_string)
     }
 
-    pub fn is_readable(&self) -> Result<bool, Error> {
+    pub fn is_readable(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_readable(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_readable_by_owner(&self) -> Result<bool, Error> {
+    pub fn is_readable_by_owner(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_readable_by_owner(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_readable_by_group(&self) -> Result<bool, Error> {
+    pub fn is_readable_by_group(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_readable_by_group(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_readable_by_others(&self) -> Result<bool, Error> {
+    pub fn is_readable_by_others(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_readable_by_others(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_readable_by_user(&self, user: &'static str) -> Result<bool, Error> {
+    pub fn is_readable_by_user(&self, user: &'static str) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_readable_by_user(self.name, user))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_writable(&self) -> Result<bool, Error> {
+    pub fn is_writable(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_writable(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_writable_by_owner(&self) -> Result<bool, Error> {
+    pub fn is_writable_by_owner(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_writable_by_owner(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_writable_by_group(&self) -> Result<bool, Error> {
+    pub fn is_writable_by_group(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_writable_by_group(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_writable_by_others(&self) -> Result<bool, Error> {
+    pub fn is_writable_by_others(&self) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_writable_by_others(self.name))
             .and_then(Output::to_bool)
     }
 
-    pub fn is_writable_by_user(&self, user: &'static str) -> Result<bool, Error> {
+    pub fn is_writable_by_user(&self, user: &'static str) -> Result<bool, error::Error> {
         self.backend
             .handle(self.provider.file.is_writable_by_user(self.name, user))
             .and_then(Output::to_bool)
     }
 
-    pub fn md5sum(&self) -> Result<String, Error> {
+    pub fn md5sum(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.md5sum(self.name))
             .and_then(Output::to_string)
     }
 
-    pub fn sha256sum(&self) -> Result<String, Error> {
+    pub fn sha256sum(&self) -> Result<String, error::Error> {
         self.backend
             .handle(self.provider.file.sha256sum(self.name))
             .and_then(Output::to_string)
@@ -189,6 +193,20 @@ pub extern "C" fn resource_file_free(ptr: *mut File) {
         Box::from_raw(ptr);
     }
 }
+
+#[no_mangle]
+pub extern "C" fn resource_file_error_description(ptr: *const File) -> *const c_char {
+    let f = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+
+    match f.error {
+        Some(ref e) => CString::new(e.description()).unwrap().into_raw(),
+        None => std::ptr::null(),
+    }
+}
+
 
 #[no_mangle]
 pub extern "C" fn resource_file_mode(ptr: *const File) -> uint32_t {
