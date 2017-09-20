8b550044ff1bb4288b072a15c9cbb67a86d029bd
Gosuke Miyashita <gosukenator@gmail.com>
2017-09-20 17:47:50 +0900

    Fix error handling of resource_file_is_readable_by_user()

--- src/resource/file.rs
+++ src/resource/file.rs
@@ -462,12 +462,10 @@ pub extern "C" fn resource_file_is_readable_by_others(ptr: *mut File) -> int32_t
 }
 
 #[no_mangle]
-pub extern "C" fn resource_file_is_readable_by_user(ptr: *const File,
-                                                    u: *const c_char)
-                                                    -> uint32_t {
+pub extern "C" fn resource_file_is_readable_by_user(ptr: *mut File, u: *const c_char) -> int32_t {
     let f = unsafe {
         assert!(!ptr.is_null());
-        &*ptr
+        &mut *ptr
     };
 
     let c_str = unsafe {
@@ -476,10 +474,12 @@ pub extern "C" fn resource_file_is_readable_by_user(ptr: *const File,
     };
 
     let user = c_str.to_str().unwrap();
-    if f.is_readable_by_user(user).unwrap() {
-        1
-    } else {
-        0
+    match f.is_readable_by_user(user) {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            f.error = Some(e);
+            return -1;
+        }
     }
 }
 
