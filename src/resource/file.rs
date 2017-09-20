88a93cffbd5190001042d0143554bb17469c90e6
Gosuke Miyashita <gosukenator@gmail.com>
2017-09-20 13:35:51 +0900

    Fix error handling of resource_file_is_symlink()

--- src/resource/file.rs
+++ src/resource/file.rs
@@ -335,13 +335,19 @@ pub extern "C" fn resource_file_is_socket(ptr: *mut File) -> int32_t {
 }
 
 #[no_mangle]
-pub extern "C" fn resource_file_is_symlink(ptr: *const File) -> uint32_t {
+pub extern "C" fn resource_file_is_symlink(ptr: *mut File) -> int32_t {
     let f = unsafe {
         assert!(!ptr.is_null());
-        &*ptr
+        &mut *ptr
     };
 
-    if f.is_symlink().unwrap() { 1 } else { 0 }
+    match f.is_symlink() {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            f.error = Some(e);
+            return -1;
+        }
+    }
 }
 
 #[no_mangle]
