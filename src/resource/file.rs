2f3d03d06ba985dcfcaf1e45ea09db95f7798d2c
Gosuke Miyashita <gosukenator@gmail.com>
2017-09-20 16:52:51 +0900

    Fix error handling of resource_file_is_readable_by_owner()

--- src/resource/file.rs
+++ src/resource/file.rs
@@ -414,16 +414,18 @@ pub extern "C" fn resource_file_is_readable(ptr: *mut File) -> int32_t {
 }
 
 #[no_mangle]
-pub extern "C" fn resource_file_is_readable_by_owner(ptr: *const File) -> uint32_t {
+pub extern "C" fn resource_file_is_readable_by_owner(ptr: *mut File) -> int32_t {
     let f = unsafe {
         assert!(!ptr.is_null());
-        &*ptr
+        &mut *ptr
     };
 
-    if f.is_readable_by_owner().unwrap() {
-        1
-    } else {
-        0
+    match f.is_readable_by_owner() {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            f.error = Some(e);
+            return -1;
+        }
     }
 }
 
