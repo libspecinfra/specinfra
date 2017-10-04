3e22d0ba1e26d39df5758e133c14e144002ae359
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-04 14:01:17 +0900

    Change theme and disable source link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -169,3 +169,4 @@ texinfo_documents = [
 
 
 
+html_show_sourcelink = False
