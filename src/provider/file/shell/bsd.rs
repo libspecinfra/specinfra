2bd41ac42076c876d387610761e1db0816c34cba
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-06 14:23:37 +0900

    Implementing file shell bsd provider

--- src/provider/file/shell/bsd.rs
+++ src/provider/file/shell/bsd.rs
@@ -4,6 +4,7 @@ use backend::Backend;
 use provider::error::Error;
 use provider::Output;
 use provider::file::shell::ShellProvider;
+use provider::file::shell::unix::Unix;
 
 #[derive(Clone, Debug)]
 pub struct Bsd;
@@ -16,7 +17,55 @@ impl ShellProvider for Bsd {
         Ok(Output::I32(m))
     }
 
+    fn exist(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.exist(name, b)
+    }
+
+    fn is_file(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_file(name, b)
+    }
+
+    fn is_directory(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_directory(name, b)
+    }
+
+    fn is_block_device(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_block_device(name, b)
+    }
+
+    fn is_character_device(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_character_device(name, b)
+    }
+
+    fn is_pipe(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_pipe(name, b)
+    }
+
+    fn is_socket(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_socket(name, b)
+    }
+
+    fn is_symlink(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.is_symlink(name, b)
+    }
+
+    fn contents(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        Unix.contents(name, b)
+    }
+
     fn box_clone(&self) -> Box<ShellProvider> {
         Box::new((*self).clone())
+    }
+
+    fn owner(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        let c = format!("stat -f%Su {}", name);
+        let res = try!(b.run_command(&c));
+        Ok(Output::Text(res.stdout))
+    }
+
+    fn group(&self, name: &str, b: &Backend) -> Result<Output, Error> {
+        let c = format!("stat -f%Sg {}", name);
+        let res = try!(b.run_command(&c));
+        Ok(Output::Text(res.stdout))
     }
 }
