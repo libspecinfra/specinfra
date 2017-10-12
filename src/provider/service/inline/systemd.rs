9c2d989bcd104e97b388fa435cf6a4dbf0794f59
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-12 18:11:06 +0900

    Implement service.is_running()

--- src/provider/service/inline/systemd.rs
+++ src/provider/service/inline/systemd.rs
@@ -3,6 +3,7 @@ pub extern crate dbus;
 use std::result::Result;
 
 use self::dbus::{Connection, BusType, Message, Path};
+use self::dbus::arg::Variant;
 
 use provider::service::inline::InlineProvider;
 use provider::Output;
@@ -13,8 +14,8 @@ pub struct Systemd;
 
 impl InlineProvider for Systemd {
     fn is_running(&self, name: &str) -> Result<Output, Error> {
-
-        Ok(Output::Bool(true))
+        let state = try!(self.get_active_state(name));
+        Ok(Output::Bool(state == "active"))
     }
 
     fn box_clone(&self) -> Box<InlineProvider> {
@@ -23,17 +24,28 @@ impl InlineProvider for Systemd {
 }
 
 impl Systemd {
-    fn get_active_state(&self, name: &str) -> Result<&str, Error> {
+    fn get_active_state(&self, name: &str) -> Result<String, Error> {
         let c = try!(Connection::get_private(BusType::System));
 
         let m = try!(Message::new_method_call("org.freedesktop.systemd1",
                                               "/org/freedesktop/systemd1",
                                               "org.freedesktop.systemd1.Manager",
-                                              "GetUnit"));
+                                              "GetUnit"))
+            .append1(name);
 
         let r = try!(c.send_with_reply_and_block(m, 2000));
         let object_path: Path = try!(r.read1());
 
-        Ok("active")
+        let m = try!(Message::new_method_call("org.freedesktop.systemd1",
+                                              object_path,
+                                              "org.freedesktop.DBus.Properties",
+                                              "Get"))
+            .append2("org.freedesktop.systemd1.Unit", "ActiveState");
+
+        let r = try!(c.send_with_reply_and_block(m, 2000));
+        let s: Variant<&str> = try!(r.read1());
+        let active_state = s.0;
+
+        Ok(active_state.to_string())
     }
 }
