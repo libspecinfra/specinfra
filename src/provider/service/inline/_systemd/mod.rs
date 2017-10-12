b27350f00cd1705b5fbb954351bcc1e4f781cf24
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-12 19:50:43 +0900

    Implement dummy systemd provider

--- src/provider/service/inline/_systemd/mod.rs
+++ src/provider/service/inline/_systemd/mod.rs
@@ -1,1 +1,12 @@
+use provider::service::inline::InlineProvider;
+
+#[derive(Clone, Debug)]
+pub struct Systemd;
+
+impl InlineProvider for Systemd {
+    fn box_clone(&self) -> Box<InlineProvider> {
+        Box::new((*self).clone())
+    }
+}
+
 pub mod dbus;
