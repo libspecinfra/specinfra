ae363c344e3a21f4237a0bf84cdbd37f441f2617
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-23 16:02:44 +0900

    Add resource_service_free() and resource_service_is_running()

--- src/resource/service.rs
+++ src/resource/service.rs
@@ -1,3 +1,8 @@
+use libc::{c_char, int32_t};
+use std;
+use std::ffi::CString;
+use std::error::Error;
+
 use backend::Backend;
 use provider::error;
 use provider::Output;
@@ -66,5 +71,46 @@ impl<'a> Service<'a> {
         self.backend
             .handle(self.provider.restart(self.name))
             .and_then(Output::to_bool)
+    }
+}
+
+// Wrapper functions for FFI
+
+#[no_mangle]
+pub extern "C" fn resource_service_free(ptr: *mut Service) {
+    if ptr.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn resource_service_error_description(ptr: *const Service) -> *const c_char {
+    let s = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+
+    match s.error {
+        Some(ref e) => CString::new(e.description()).unwrap().into_raw(),
+        None => std::ptr::null(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn resource_service_is_running(ptr: *mut Service) -> int32_t {
+    let s = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+
+    match s.is_running() {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            s.error = Some(e);
+            return -1;
+        }
     }
 }
