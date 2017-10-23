8ec540c23e3ce321e3b1e3295e9c218fe5c51dac
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-23 20:49:06 +0900

    Add a wrapper funcion for service.enable()

--- src/resource/service.rs
+++ src/resource/service.rs
@@ -130,3 +130,19 @@ pub extern "C" fn resource_service_is_enabled(ptr: *mut Service) -> int32_t {
         }
     }
 }
+
+#[no_mangle]
+pub extern "C" fn resource_service_enable(ptr: *mut Service) -> int32_t {
+    let s = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+
+    match s.enable() {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            s.error = Some(e);
+            return -1;
+        }
+    }
+}
