70879f06d30832d764310f17ee90ddaae8058a8b
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-24 18:35:25 +0900

    Add a wrapper function of service.restart()

--- src/resource/service.rs
+++ src/resource/service.rs
@@ -178,3 +178,19 @@ pub extern "C" fn resource_service_reload(ptr: *mut Service) -> int32_t {
         }
     }
 }
+
+#[no_mangle]
+pub extern "C" fn resource_service_restart(ptr: *mut Service) -> int32_t {
+    let s = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+
+    match s.restart() {
+        Ok(f) => if f { 1 } else { 0 },
+        Err(e) => {
+            s.error = Some(e);
+            return -1;
+        }
+    }
+}
